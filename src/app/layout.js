b2a71a8d76894bd708926ea36b1c73fdf974a901
KinshukSaini <kinshukrosh@gmail.com>
2025-11-04 22:01:47 +0530

    font family issue fixed

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,22 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Koulen } from 'next/font/google';
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+const koulen = Koulen({
+  subsets: ['latin'], 
+  weight: '400', // Koulen typically only has one weight
+  display: 'swap',
+  variable: '--font-koulen', // Assign a CSS variable for global use
 });
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
 
 export const metadata = {
   title: "Kinshuk Saini",
@@ -20,7 +27,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${koulen.variable} antialiased`}
       >
         {children}
       </body>
