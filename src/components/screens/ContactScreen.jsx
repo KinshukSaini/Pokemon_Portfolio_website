b412542862ece330ef62fe7f7b0f72ca88019660
KinshukSaini <kinshukrosh@gmail.com>
2026-01-25 20:12:16 +0530

    contact screen now responsive

--- src/components/screens/ContactScreen.jsx
+++ src/components/screens/ContactScreen.jsx
@@ -2,40 +2,38 @@ import React, { useState } from "react";
 import Image from "next/image";
 const ContactScreen = () => {
   return (
-    <div className="z-90 p-[1%] pt-[2%] h-[90%] w-[65%] gap-auto bg-[#DBCBB0] flex items-center justify-around rounded-[2vh] text-box-shadow relative">
-      <Image src="masking_tape.svg" width={75} height={25} alt="Masking Tape" className="absolute -translate-x-1/2 left-1/12 top-1/8 z-99" />
+    <div className="z-90 p-[1%] pt-[2%] lg:h-[90%] h-auto w-full max-w-[1000px] lg:bg-[#DBCBB0] flex lg:flex-row flex-col items-center lg:justify-around justify-start rounded-[2vh] lg:text-box-shadow relative overflow-hidden">
+      <Image src="masking_tape.svg" width={75} height={25} alt="Masking Tape" className="lg:absolute lg:-translate-x-1/2 lg:left-1/10 lg:top-1/7 z-99 lg:block hidden" />
 
-      <div className="bg-[#DBCBB0] h-full w-[38%] flex flex-col">
-        <div className="bg-gray-200 left-1/13 top-3/10 -translate-y-1/4 h-[35vh] w-[35vh] absolute">
+      <div className="lg:bg-[#DBCBB0] lg:h-full h-auto w-full flex flex-col items-center lg:items-start">
+        <div className="bg-gray-200 lg:left-1/13 lg:top-3/10 lg:-translate-y-1/4 lg:h-[35vh] lg:w-[35vh] h-[28vh] w-[28vh] lg:absolute lg:relative relative">
           <Image
             src="/demo_profile.png"
             width={500}
             height={500}
             alt="Contact Me"
-            className="object-cover h-full w-full origin-top-left -rotate-[7deg]"
+            className="object-cover h-full w-full origin-top-left lg:-rotate-[7deg]"
           />
         </div>
 
-        <div className="flex flex-row absolute left-1/2 -translate-x-1/2 bottom-1/8">
-          <div className="bg-[#E2BE82] h-[80px] w-[90px] rounded-[1.7vh] flex items-center justify-center icon-box-shadow mx-[2vh]">
-            <Image src="/git.svg" width={50} height={50} alt="GitHub" />
+        <div className="flex flex-row flex-wrap lg:absolute lg:left-1/2 lg:-translate-x-1/2 lg:bottom-1/8 gap-3 lg:gap-[2vh] justify-center m-4">
+          <div className="bg-[#E2BE82] lg:h-[80px] lg:w-[90px] h-[56px] w-[64px] rounded-[1.7vh] flex items-center justify-center icon-box-shadow">
+            <Image src="/git.svg" width={40} height={40} alt="GitHub" />
           </div>
-          <div className="bg-[#E2BE82] h-[80px] w-[90px] rounded-[1.7vh] flex items-center justify-center icon-box-shadow mx-[2vh]">
-            <Image src="/linkedin.svg" width={50} height={50} alt="LinkedIn" />
+          <div className="bg-[#E2BE82] lg:h-[80px] lg:w-[90px] h-[56px] w-[64px] rounded-[1.7vh] flex items-center justify-center icon-box-shadow">
+            <Image src="/linkedin.svg" width={40} height={40} alt="LinkedIn" />
           </div>
-          <div className="bg-[#E2BE82] h-[80px] w-[90px] rounded-[1.7vh] flex items-center justify-center icon-box-shadow mx-[2vh]">
-            <Image src="/gmail.svg" width={50} height={50} alt="Gmail" />
+          <div className="bg-[#E2BE82] lg:h-[80px] lg:w-[90px] h-[56px] w-[64px] rounded-[1.7vh] flex items-center justify-center icon-box-shadow">
+            <Image src="/gmail.svg" width={40} height={40} alt="Gmail" />
           </div>
-          <div className="bg-[#E2BE82] h-[80px] w-[90px] rounded-[1.7vh] flex items-center justify-center icon-box-shadow mx-[2vh]">
-            <Image src="/X_logo.svg" width={50} height={50} alt="X.com" />
+          <div className="bg-[#E2BE82] lg:h-[80px] lg:w-[90px] h-[56px] w-[64px] rounded-[1.7vh] flex items-center justify-center icon-box-shadow">
+            <Image src="/X_logo.svg" width={40} height={40} alt="X.com" />
           </div>
         </div>
       </div>
       {/* ending phrase */}
-      <div className="text-[#40382B] w-[43%] absolute right-1/6 
-      max-[1500px]:right-1/10
-      translate-x-1/4 top-1/4 -translate-y-1/4 text-[1.6rem] lg:text-[1.6rem] md:text-[1.2rem] sm:text-[1rem]  pr-4">
-      I am actively seeking new project opportunities and am eager to connect with like-minded individuals and teams. If you have a project in mind or an idea to discuss, let's get in touch! 
+      <div className="lg:text-[#40382B] text-white lg:w-[43%] w-full lg:absolute lg:right-1/6 lg:translate-x-1/4 lg:top-1/4 lg:-translate-y-1/4 lg:text-[1.8rem] text-[1rem] px-4 lg:pr-4 text-center lg:text-left mt-4 lg:mt-0">
+        I am actively seeking new project opportunities and am eager to connect with like-minded individuals and teams. If you have a project in mind or an idea to discuss, let's get in touch!
       </div>
     </div>
   );
--- src/components/screens/HomeScreen.jsx
+++ src/components/screens/HomeScreen.jsx
@@ -63,10 +63,10 @@ const HomeScreen = () => {
   };
 
   return (
-    <div className="z-10 absolute flex flex-row items-center justify-center h-full w-full gap-[5%]">
-      <div className="h-[90%] w-[30%]">
+    <div className="z-10 absolute flex flex-col xl:flex-row items-center justify-center h-full w-full gap-[5%]">
+      <div className="h-[40%] xl:h-[90%] w-[80%] xl:w-[30%]">
         {/* Circles */}
-        <div className="absolute z-9 left-1/15 top-1/2 -translate-y-1/2">
+        <div className="absolute z-9 left-1/2 xl:left-1/15 top-1/4 xl:top-1/2 -translate-x-1/2 xl:-translate-x-0 -translate-y-1/2">
           <Circles />
           {/* Speaker Image */}
           <div className="absolute z-10 left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
@@ -76,7 +76,7 @@ const HomeScreen = () => {
                 width={501}
                 height={771}
                 alt="Doctor Oak Thinking"
-                className="scale-150"
+                className="scale-110 xl:scale-150"
               />
             ) : isSpeaking ? (
               <Image
@@ -84,7 +84,7 @@ const HomeScreen = () => {
                 width={501}
                 height={772}
                 alt="Doctor Oak Speaking"
-                className="scale-150"
+                className="scale-110 xl:scale-150"
               />
             ) : (
               <Image
@@ -92,14 +92,14 @@ const HomeScreen = () => {
                 width={501}
                 height={771}
                 alt="Doctor Oak Passive"
-                className="scale-150"
+                className="scale-110 xl:scale-150"
               />
             )}
           </div>
         </div>
       </div>
 
-      <div className="h-[90%] w-[60%]">
+      <div className="h-[60%] xl:h-[90%] w-[90%] xl:w-[60%]">
         {/* Chat Box */}
         <div 
           className="relative w-full max-w-[1000px] m-auto cursor-pointer"
@@ -108,7 +108,7 @@ const HomeScreen = () => {
           <Image src="/chatBox2.png" width={1000} height={400} alt="Chat Box" />
 
           {/* overlay div on top of the image */}
-          <div ref={textContainerRef} className="absolute inset-0 z-10 m-12 mr-20 left-[1%] text-2xl overflow-hidden">
+          <div ref={textContainerRef} className="absolute inset-0 z-10 m-8 xl:m-12 mr-16 xl:mr-20 left-[1%] text-xl xl:text-2xl overflow-hidden">
             {isLoading ? (
               <div className="flex items-center justify-center h-full text-center text-gray-400 animate-pulse">
                 ...
@@ -118,7 +118,7 @@ const HomeScreen = () => {
                 {displayedText}
                 {isFinished && hasMorePages && (
                   <div className="absolute bottom-2 right-4 animate-bounce">
-                    <span className="text-4xl text-gray-700">▼</span>
+                    <span className="text-3xl xl:text-4xl text-gray-700">▼</span>
                   </div>
                 )}
               </>
@@ -126,20 +126,20 @@ const HomeScreen = () => {
           </div>
         </div>
 
-        <div className=" flex items-center justify-center gap-4 m-20">
+        <div className="flex items-center justify-center gap-4 m-10 xl:m-20">
           <input
             type="text"
             value={userInput}
             onChange={(e) => setUserInput(e.target.value)}
             onKeyPress={handleKeyPress}
-            className="flex-1 rounded-md border-0 border-b-2 border-transparent text-white focus:border-blue-500 focus:border-b-2 focus:outline-none focus:ring-0 transition-all text-2xl pb-4"
+            className="flex-1 rounded-md border-0 border-b-2 border-transparent text-white focus:border-blue-500 focus:border-b-2 focus:outline-none focus:ring-0 transition-all text-xl xl:text-2xl pb-4"
             placeholder="ask something..."
           />
           {userInput.trim() !== "" && (
             <button 
               onClick={handleSendMessage}
               disabled={isLoading}
-              className="bg-blue-500/20 text-2xl hover:bg-blue-400/30 hover:duration-200 text-white px-4 py-2 rounded-md disabled:opacity-50 disabled:cursor-not-allowed"
+              className="bg-blue-500/20 text-xl xl:text-2xl hover:bg-blue-400/30 hover:duration-200 text-white px-4 py-2 rounded-md disabled:opacity-50 disabled:cursor-not-allowed"
             >
               {isLoading ? "..." : "GO"}
             </button>
